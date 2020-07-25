85c48fc4f6a163cd1e1172ef7981133c378ba32b
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-25 05:29:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ var listItems = document.getElementById("list");
 
 var item = document.createElement("li");
 
+function AddTodoItem() {
+    var listItem = document.getElementById("listItem").value;
+    var itemNode = document.createTextNode(listItem);
+    item.appendChild(itemNode);
+    listItems.appendChild(item);
+    console.log(listItem);
+    console.log(listItems);
+}
+
