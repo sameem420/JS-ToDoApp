2f123cd9c2a11ea75b6b53f0ca7fe08a8787516e
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-25 05:41:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function AddTodoItem() {
     var todoItem = document.getElementById("todoItem").value;
     var li = document.createElement("li");
     var todoItemText = document.createTextNode(todoItem);
+    var editButton = document.createElement("button");
+    var deleteButton = document.createElement("button");
     todoItem.value = "";
     li.appendChild(todoItemText);
     myTodoList.appendChild(li);
